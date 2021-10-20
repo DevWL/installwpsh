12f3941fed253cbe030aaf3d5b470e6b3fb56d1c
DevWL <w.liszkiewicz@gmail.com>
2021-10-21 01:43:38 +0200

    v1.5.15

--- installwp.sh
+++ installwp.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Version: v1.4.1
-# This script is currently under development.
+# This script is currently under development (alpha version).
+# Next version will proceed as fallow: version v2.0.0 (beta), version v3.0.0 (RC) and finely v4.0.0 (first stable).
+# Feel free to contribute to fix anny spoted issues.
 #
-# No Warranty! This script comes with no warantty of any kind. 
-# Use can use this script at your your own risk.
-# NOTICE! Note the use of "rm -rf" in the script which if missued can case data lose. Always validate and verify the script before you run it!.
+# WARNING! No Warranty! This script comes with no warantty of any kind. 
+# You can use this script at your own risk.
+# NOTICE! Note the use of "rm -rf" in the script which if missued can cause data lost. Always validate and verify the script before you run it!.
 # 
 # @Author: Wiktor Liszkiewicz
 # @Email: w.liszkiewicz@gmail.com
@@ -32,43 +34,24 @@
 ############################################# run LARAGON ###################################################################################
     # manualy
 
+#############################################  INITIAL SETUP #################################################################################
+    read -p 'default [testsite]: ' PROJECTNAME
+    PROJECTNAME=${PROJECTNAME:-"testsite"}
 
-#############################################  PLUGIN AND THEME CLEANUP #################################################################################
+    echo ""
+    echo "# Project e-email "
+    read -p 'default [w.liszkiewicz@gmail.com]: ' WPEMAIL
+    WPEMAIL=${WPEMAIL:-"w.liszkiewicz@gmail.com"} # <<<<< edit default email 
 
-# whipe out all themes
-echo "# Type 'y' to remove all themes! or Press enter for default [n]: "
-read -p 'Press ENTER for  default [n]: ' WIPETHEMES
-WIPETHEMES=${WIPETHEMES:-"n"}
+    SERVERWWW="/c/laragon/www" # <<<<< edit to fit your needs
+    PROJECTROOT=$SERVERWWW/$PROJECTNAME
 
-if [ "$WIPETHEMES" = "y" ]
-then
-    rm -rf ${PROJECTROOT}/wp-content/themes/*
-    cp ${PROJECTROOT}/wp-content/plugins/index.php ${PROJECTROOT}/wp-content/themes
-else
-    rm -rf ${PROJECTROOT}/wp-content/themes/{twentynineteen,twentyseventeen,twentysixteen,twentytwenty,twentytwentyone}
-fi
+    # Setup site folder, replace all $PROJECTNAME with $PROJECTNAME
+    mkdir ${PROJECTROOT}
+    cd ${PROJECTROOT}
 
-# whipe out all plugins
-echo '# Type y to remove all plugins! Ppress enter for default [n]: '
-read -p 'Press ENTER for  default [n]: ' WIPEPLUGINS
-WIPEPLUGINS=${WIPEPLUGINS:-"n"}
-
-if [ "$WIPEPLUGINS" = "y" ]
-then
-    rm -rf ${PROJECTROOT}/wp-content/plugins/*
-    cp ${PROJECTROOT}/wp-content/themes/index.php ${PROJECTROOT}/wp-content/plugins
-else
-    rm -rf ${PROJECTROOT}/wp-content/plugins/{hello.php,akismet}
-fi
-
-
-#############################################  DATABASE MYSQL #################################################################################
-# # Setup MySQL Database
-# mysql.server start # use on linux
-mysql -u root -e "create database ${PROJECTNAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
-mysql -u root -e "create user '${PROJECTNAME}'@'localhost' identified by password ''";
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${PROJECTNAME}.* TO ${PROJECTNAME}@localhost";
-mysql -u root -e "FLUSH PRIVILEGES";
+    echo "# Get fresh files? Requiered when runing first time!"
+    read -p 'y/n default [n]' UPDATEFILE
 
 #############################################  DOWNLOAD WP || LOAD LOCAL FILE  #################################################################################
 #https://superuser.com/questions/493640/how-to-retry-connections-with-wget
@@ -94,18 +77,13 @@ mysql -u root -e "FLUSH PRIVILEGES";
     cd ./wordpress || exit $?
     cd ${PROJECTROOT}/wordpress # another way
 
+    mv * ${PROJECTROOT}
+    mv .* ${PROJECTROOT} 2> /dev/null #suppress errors and warnings
+    cd ..
+    rmdir ./wordpress
 
-# ######################################## create themes LIB ########################################
-THEMESLIB=${SERVERWWW}/wp-themes
-mkdir ${SERVERWWW}/wp-themes 2> /dev/null #suppress errors and warnings
-cd ${THEMESLIB}
 
-if [ "$UPDATEFILE" = "y" ]
-then
-    wget -q -O stable.zip http://github.com/toddmotto/html5blank/archive/stable.zip || curl -O http://github.com/toddmotto/html5blank/archive/stable.zip # use wget or curl -O
-    # curl -O http://github.com/toddmotto/html5blank/archive/stable.zip
-    cp -r /c/users/symfony/downloads/Divi.zip $THEMESLIB
-fi
+#############################################  PLUGIN AND THEME CLEANUP #################################################################################
 
     # Whipe out all themes
         echo '# Removeing default themes: '
@@ -116,23 +94,18 @@ fi
         echo '# Removeing default plugins: '
         rm -rf ${PROJECTROOT}/wp-content/plugins/{hello.php,akismet}
 
-# unzip all files
-unzip -qqo \*.zip
 
-cp -r ${THEMESLIB}/* ${PROJECTROOT}/wp-content/themes
-rm ${PROJECTROOT}/wp-content/themes/*.zip
-mv ${PROJECTROOT}/wp-content/themes/html5blank-stable/ ${PROJECTROOT}/wp-content/themes/$PROJECTNAME
-
-# # Install and activate theme and plugins
-cd ${PROJECTROOT}
-wp-cli.phar theme activate Divi || wp-cli.phar theme activate $PROJECTNAME
+#############################################  DATABASE MYSQL #################################################################################
+# # Setup MySQL Database
+# mysql.server start # use on linux
+    mysql -u root -e "create database ${PROJECTNAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci";
+    mysql -u root -e "create user '${PROJECTNAME}'@'localhost' identified by password ''";
+    mysql -u root -e "GRANT ALL PRIVILEGES ON ${PROJECTNAME}.* TO ${PROJECTNAME}@localhost";
+    mysql -u root -e "FLUSH PRIVILEGES";
 
 
-# ######################################## create plugin LIB ########################################
-cd $SERVERWWW
-PLUGINLIB=${SERVERWWW}/wp-plugins 
-mkdir $PLUGINLIB 2> /dev/null #suppress errors and warnings
-cd $PLUGINLIB
+
+    # # Create config file # note I have used .dv instead of laragon default .test local domain extension
 
     WPUSER="Laragon"
     WPPASS="silentisgold"
@@ -144,8 +117,6 @@ cd $PLUGINLIB
     wp-cli.phar core install --url=${WPURL} --title=${PROJECTNAME} --admin_user=${WPUSER} --admin_password=${WPPASS} --admin_email=${WPEMAIL}
     # wp https://make.wordpress.org/cli/handbook/guides/installing/
 
-#Unzip all zip files
-unzip -qqo \*.zip
 
 # ######################################## create themes LIB ########################################
     THEMESLIB=${SERVERWWW}/wp-themes
@@ -313,39 +284,40 @@ unzip -qqo \*.zip
 
 ###################### USE WP-CLI - ADD REMOVE PAGES/POSTS ######################
 # Remove posts/pages
-wp-cli.phar post delete 1 --force #Hello World!
-wp-cli.phar post delete 2 --force #Sample Page
+    cd ${PROJECTROOT}
+    wp-cli.phar post delete 1 --force #Hello World!
+    wp-cli.phar post delete 2 --force #Sample Page
 
 # Add Pages - Set your own page name
-wp-cli.phar post create --post_type=page --post_status=publish --post_title="Strona domowa" # replace post_title value
-wp-cli.phar post create --post_type=page --post_status=draft --post_title=Kontakt # replace post_title value
-wp-cli.phar post create --post_type=page --post_status=draft --post_title=Blog # replace post_title value
-wp-cli.phar post create --post_type=page --post_status=draft --post_title=Produkty # replace post_title value
-wp-cli.phar post create --post_type=page --post_status=draft --post_title=Usługi # replace post_title value
-wp-cli.phar post create --post_type=page --post_status=draft --post_title=Galeria # replace post_title value
-wp-cli.phar post create --post_type=page --post_status=draft --post_title=Reaizacje # replace post_title value
-wp-cli.phar option update page_on_front 4 # replace 4 with page id you want to set as homepage # all options to set available are listed at domainname/wp-admin/options.php
+    cd ${PROJECTROOT}
+    wp-cli.phar post create --post_type=page --post_status=draft --post_title="Home Page" # replace post_title value
+    wp-cli.phar post create --post_type=page --post_status=draft --post_title=Contact # replace post_title value
+    wp-cli.phar post create --post_type=page --post_status=draft --post_title=Blog # replace post_title value
+    wp-cli.phar post create --post_type=page --post_status=draft --post_title=Services # replace post_title value
+    wp-cli.phar post create --post_type=page --post_status=draft --post_title=Gallery # replace post_title value
+    wp-cli.phar post create --post_type=page --post_status=draft --post_title=Portfolio # replace post_title value
+    # wp-cli.phar option update page_on_front 4 # replace 4 with page id you want to set as homepage # all options to set available are listed at domainname/wp-admin/options.php <<< This cose some issue with loading styles for homepage with DIVI theme
 
 ################### USE LARAGON CLI - LARAGON RELOAD AND OPEN ######################
 # https://laragon.org/docs/cli.html
-LARAGONPATH="/c/laragon" # chnge this line if your instalation is placed in different file
-${LARAGONPATH}/laragon reload
-start https://${PROJECTNAME}.dv
-start https://${PROJECTNAME}.dv/wp-admin
+    LARAGONPATH="/c/laragon" # chnge this line if your instalation is placed in different file
+    ${LARAGONPATH}/laragon reload
+    start https://${PROJECTNAME}.dv
+    start https://${PROJECTNAME}.dv/wp-admin
 
 ###################### Output DV login info ######################
-cd ${PROJECTROOT}
-echo > "_dv_login.txt"
-echo '' | tee -a "_dv_login.txt"
-echo "------------ DB ------------" | tee -a "_dv_login.txt"
-echo "Database: ${PROJECTNAME}" | tee -a "_dv_login.txt"
-echo "DB User: ${PROJECTNAME}" | tee -a "_dv_login.txt"
-echo "DB Pass: ''" | tee -a "_dv_login.txt"
+    cd ${PROJECTROOT}
+    echo > "_dv_login.txt"
+    echo '' | tee -a "_dv_login.txt"
+    echo "------------ DB ------------" | tee -a "_dv_login.txt"
+    echo "Database: ${PROJECTNAME}" | tee -a "_dv_login.txt"
+    echo "DB User: ${PROJECTNAME}" | tee -a "_dv_login.txt"
+    echo "DB Pass: ''" | tee -a "_dv_login.txt"
 
-echo '' | tee -a "_dv_login.txt"
-echo "------------ WP ------------" | tee -a "_dv_login.txt"
-echo "WP Url: ${WPURL}" | tee -a "_dv_login.txt"
-echo "WP User: ${WPUSER}" | tee -a "_dv_login.txt"
-echo "WP Pass: ${WPPASS}" | tee -a "_dv_login.txt"
-echo "WP Email: ${WPEMAIL}" | tee -a "_dv_login.txt"
+    echo '' | tee -a "_dv_login.txt"
+    echo "------------ WP ------------" | tee -a "_dv_login.txt"
+    echo "WP Url: ${WPURL}" | tee -a "_dv_login.txt"
+    echo "WP User: ${WPUSER}" | tee -a "_dv_login.txt"
+    echo "WP Pass: ${WPPASS}" | tee -a "_dv_login.txt"
+    echo "WP Email: ${WPEMAIL}" | tee -a "_dv_login.txt"
 
